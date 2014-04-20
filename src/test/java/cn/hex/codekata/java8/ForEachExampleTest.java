55f31d4713516b2e73c292ad4ae86378bcd9da62
He-Xin <hexinmthuy@gmail.com>
2014-04-20 21:42:38 +0800

    use lamda expression

--- src/test/java/cn/hex/codekata/java8/ForEachExampleTest.java
+++ src/test/java/cn/hex/codekata/java8/ForEachExampleTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
 
 import static org.junit.Assert.assertEquals;
 
@@ -15,15 +14,12 @@ import static org.junit.Assert.assertEquals;
 public class ForEachExampleTest {
     @Test
     public void testIterateWithForEach() throws Exception {
-        ForEachExample<Integer> example = new ForEachExample<Integer>(Arrays.asList(1, 2, 3));
+        ForEachExample<Integer> example = new ForEachExample<>(Arrays.asList(1, 2, 3));
 
-        final List<Integer> result = new ArrayList<Integer>();
+        final List<Integer> result = new ArrayList<>();
 
-        example.iterateWithForEach(new Consumer<Integer>() {
-            @Override
-            public void accept(Integer integer) {
-                result.add(integer * 2);
-            }
+        example.iterateWithForEach(integer -> {
+            result.add(integer * 2);
         });
 
         assertEquals(Arrays.asList(2, 4, 6), result);
