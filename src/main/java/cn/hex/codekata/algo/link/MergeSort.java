6926e18f5afb0a78ff850b2e6005ea0a99e9653a
He-Xin <hexinmthuy@gmail.com>
2014-04-27 15:42:03 +0800

    add some comments for merge sort of single linked list

--- src/main/java/cn/hex/codekata/algo/link/MergeSort.java
+++ src/main/java/cn/hex/codekata/algo/link/MergeSort.java
@@ -18,16 +18,17 @@ public class MergeSort {
     }
 
     public static <T extends Comparable<T>> void sortByStep(Link<T> start, int step) {
-
         while(start.getNext() != null) {
             start = doMergeSort(start, start.getNext(), step);
         }
     }
 
+    // merge two sorted linked list
     public static <T extends Comparable<T>> Link<T> doMergeSort(Link<T> start, Link<T> l, int step) {
         Link<T> i = l, j= l, elem = null;
 
         for (int k = 0; k < step; k++) {
+            // return last element if size of linked list is less than given step
             if (j.getNext() == null) {
                 start.setNext(i);
                 return j;
@@ -35,10 +36,10 @@ public class MergeSort {
             j = j.getNext();
         }
 
+        // link element with smallest value to previous element
         start.setNext(i.getValue().compareTo(j.getValue()) <= 0 ? i : j);
 
         int ci = 0, cj = 0;
-
         while (ci < step && cj < step) {
             if (i.getValue().compareTo(j.getValue()) <= 0) {
                 if (elem != null) {
@@ -65,6 +66,7 @@ public class MergeSort {
             }
         }
 
+        // navigate to the last element and return
         if ( ci < step) {
             while (ci < step -1) {
                 i = i.getNext();
