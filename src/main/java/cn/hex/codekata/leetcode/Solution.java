a9dfe66950a02eb5d631dd9c870345d765c05623
He-Xin <hexinmthuy@gmail.com>
2014-05-11 13:34:21 +0800

    leetcode, single number

--- src/main/java/cn/hex/codekata/leetcode/Solution.java
+++ src/main/java/cn/hex/codekata/leetcode/Solution.java
@@ -56,4 +56,13 @@ public class Solution {
         }
         return max;
     }
+
+    //Given an array of integers, every element appears twice except for one. Find that single one.
+    public static int singleNumber(int A[]) {
+        int result = A[0];
+        for(int i = 1; i < A.length; i++) {
+            result = result ^ A[i];
+        }
+        return result;
+    }
 }
--- src/test/java/cn/hex/codekata/leetcode/SolutionTest.java
+++ src/test/java/cn/hex/codekata/leetcode/SolutionTest.java
@@ -28,7 +28,7 @@ public class SolutionTest {
 
     @Test
     public void testWordBreak() {
-        Set<String> dict = new HashSet<String>();
+        Set<String> dict = new HashSet<>();
         dict.add("leet");
         dict.add("code");
 
@@ -47,5 +47,12 @@ public class SolutionTest {
 
         maxProfit = Solution.maxProfit(new int[]{1,4,2,3});
         assertThat(maxProfit, is(3));
+    }
+
+    @Test
+    public void testSingleNumber() {
+        assertThat(Solution.singleNumber(new int[]{1,2,1,2,3}), is(3));
+        assertThat(Solution.singleNumber(new int[]{3}), is(3));
+        assertThat(Solution.singleNumber(new int[]{1,2,1,2,3,4,5,4,5}), is(3));
     }
 }
