03b549c0f4c9e24647ec25d0b5d11d1eab6ef817
He-Xin <hexinmthuy@gmail.com>
2014-05-15 22:33:44 +0800

    leetcode, Minimum Depth of Binary Tree

--- src/main/java/cn/hex/codekata/leetcode/Solution.java
+++ src/main/java/cn/hex/codekata/leetcode/Solution.java
@@ -244,4 +244,16 @@ public class Solution {
         pathSums.add(leftMaxSum + node.val + rightMaxSum);
         return leftMaxSum > rightMaxSum ? node.val + leftMaxSum : node.val + rightMaxSum;
     }
+
+    //Minimum Depth of Binary Tree
+    //The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+    public static int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left == null && root.right == null) return 1;
+        int leftDepth = minDepth(root.left);
+        int rightDepth = minDepth(root.right);
+        if (leftDepth == 0) return rightDepth + 1;
+        if (rightDepth == 0) return leftDepth + 1;
+        return leftDepth < rightDepth ? leftDepth + 1 : rightDepth + 1;
+    }
 }
