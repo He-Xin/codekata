0b29d88667358c19997e0baeb59018197d61bc15
He-Xin <hexinmthuy@gmail.com>
2014-05-28 21:22:36 +0800

    leetcode, categorize problems

--- src/main/java/cn/hex/codekata/leetcode/ListNode.java
+++ src/main/java/cn/hex/codekata/leetcode/ListNode.java
@@ -11,4 +11,70 @@ public class ListNode {
         val = x;
         next = null;
     }
+
+    //Rotate list
+    /*
+     * Given a list, rotate the list to the right by k places, where k is non-negative.
+     * For example:
+     * Given 1->2->3->4->5->NULL and k = 2,
+     * return 4->5->1->2->3->NULL.
+     */
+    public static ListNode rotateRight(ListNode head, int n) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode l1 = head, l2 = head;
+        for (int i = 0; i < n; i++) {
+            l2 = l2.next;
+            if (l2 == null) {
+                l2 = head;
+            }
+        }
+
+        if (l2 == head) return head;
+
+        while (l2.next != null) {
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        //noinspection ConstantConditions
+        l2.next = head;
+        head = l1.next;
+        l1.next = null;
+
+        return head;
+    }
+
+
+    //Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+    public static TreeNode sortedListToBST(ListNode head) {
+        return sortedListToBST(head, null);
+    }
+
+    private static TreeNode sortedListToBST(ListNode head, ListNode tail) {
+        if (head == null) return null;
+        ListNode n1 = getMiddleOfSortedList(head, tail);
+        TreeNode root = new TreeNode(n1.val);
+        if (n1.next != tail) {
+            root.right = sortedListToBST(n1.next, tail);
+        }
+        if (n1 != head) {
+            root.left = sortedListToBST(head, n1);
+        }
+        return root;
+    }
+
+    private static ListNode getMiddleOfSortedList(ListNode head, ListNode tail) {
+        ListNode n1 = head, n2 = head;
+        while (n2 != tail) {
+            n2 = n2.next;
+            if (n2 != tail) {
+                n2 = n2.next;
+                n1 = n1.next;
+            }
+        }
+        return n1;
+    }
 }
--- src/main/java/cn/hex/codekata/leetcode/TreeNode.java
+++ src/main/java/cn/hex/codekata/leetcode/TreeNode.java
@@ -1,5 +1,7 @@
 package cn.hex.codekata.leetcode;
 
+import java.util.*;
+
 /**
  * Created by hex.
  */
@@ -11,5 +13,193 @@ public class TreeNode {
 
     TreeNode(int x) {
         val = x;
+    }
+
+
+    //Binary Tree Postorder Traversal
+    public static List<Integer> postOrderTraversal(TreeNode root) {
+        List<Integer> results = new ArrayList<>();
+        if (root == null) return results;
+        Stack<TreeNode> stack = new Stack<>();
+        Set<TreeNode> visited = new HashSet<>();
+        stack.push(root);
+        while (!stack.empty()) {
+            TreeNode node = stack.peek();
+            if (node.right != null && !visited.contains(node.right)) {
+                stack.push(node.right);
+            }
+            if (node.left != null && !visited.contains(node.left)) {
+                stack.push(node.left);
+            }
+            if ((node.left == null || visited.contains(node.left)) && (node.right == null || visited.contains(node.right))) {
+                results.add(node.val);
+                visited.add(node);
+                stack.pop();
+            }
+        }
+        return results;
+    }
+
+    //Binary Tree Preorder Traversal
+    public static List<Integer> preOrderTraversal(TreeNode root) {
+        List<Integer> results = new ArrayList<>();
+        if (root == null) return results;
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            results.add(node.val);
+            if (node.right != null)
+                stack.push(node.right);
+            if (node.left != null)
+                stack.push(node.left);
+        }
+
+        return results;
+    }
+
+    //Binary Tree inorder Traversal
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> results = new ArrayList<>();
+        if (root == null) return results;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode node = root;
+        stack.push(root);
+        while (!stack.empty()) {
+            while (node != null && node.left != null) {
+                node = node.left;
+                stack.push(node);
+            }
+            node = stack.pop();
+            results.add(node.val);
+            node = node.right;
+            if (node != null) {
+                stack.push(node);
+            }
+        }
+        return results;
+    }
+
+
+    // level order traversal
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> results = new ArrayList<>();
+        if (root == null) return results;
+        List<TreeNode> nodesOfSameLevel = Arrays.asList(root);
+
+        while (nodesOfSameLevel.size() > 0) {
+            List<TreeNode> nextLevel = new ArrayList<>();
+            List<Integer> levelValues = new ArrayList<>();
+            for (TreeNode node : nodesOfSameLevel) {
+                if (node.left != null) nextLevel.add(node.left);
+                if (node.right != null) nextLevel.add(node.right);
+                levelValues.add(node.val);
+            }
+            results.add(levelValues);
+            nodesOfSameLevel = nextLevel;
+        }
+        return results;
+    }
+
+    //Minimum Depth of Binary Tree
+    //The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+    public static int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left == null && root.right == null) return 1;
+        int leftDepth = minDepth(root.left);
+        int rightDepth = minDepth(root.right);
+        if (leftDepth == 0) return rightDepth + 1;
+        if (rightDepth == 0) return leftDepth + 1;
+        return leftDepth < rightDepth ? leftDepth + 1 : rightDepth + 1;
+    }
+
+    /**
+     * Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+     * For example, this binary tree is symmetric:
+     * 1
+     * / \
+     * 2   2
+     * / \ / \
+     * 3  4 4  3
+     * <p>
+     * But the following is not:
+     * 1
+     * / \
+     * 2   2
+     * \   \
+     * 3   3
+     *
+     * @param root root of tree
+     * @return true if tree is symmetric
+     */
+    public static boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+
+        mirror(left);
+
+        return isSameTree(left, right);
+    }
+
+    private static void mirror(TreeNode node) {
+        if (node == null) return;
+        TreeNode temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+        mirror(node.left);
+        mirror(node.right);
+    }
+
+    // check if two trees are same
+    public static boolean isSameTree(TreeNode r1, TreeNode r2) {
+        if (r1 == null && r2 == null) return true;
+
+        //noinspection SimplifiableIfStatement
+        if (r1 == null || r2 == null) return false;
+
+        return r1.val == r2.val && isSameTree(r1.left, r2.left) && isSameTree(r1.right, r2.right);
+
+    }
+
+    //Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
+    public static List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> results = new ArrayList<>();
+        if (root == null) return results;
+        traverse(root, sum, new ArrayList<>(), results);
+        return results;
+    }
+
+    private static void traverse(TreeNode node, int sum, List<Integer> list, List<List<Integer>> results) {
+        list.add(node.val);
+        if (node.left != null) traverse(node.left, sum, list, results);
+        if (node.right != null) traverse(node.right, sum, list, results);
+        if (node.left == null && node.right == null) {
+            int total = list.stream().mapToInt(i -> i).sum();
+            if (sum == total) {
+                results.add(new ArrayList<>(list));
+            }
+        }
+        list.remove(list.size() - 1);
+    }
+
+    //Given a binary tree, find the maximum path sum.The path may start and end at any node in the tree.
+    public static int maxPathSum(TreeNode root) {
+        List<Integer> pathSums = new ArrayList<>();
+        if (root == null) return 0;
+        maxPathSumFromNode(root, pathSums);
+        return Collections.max(pathSums);
+    }
+
+    private static int maxPathSumFromNode(TreeNode node, List<Integer> pathSums) {
+        if (node == null) return 0;
+        int leftMaxSum = maxPathSumFromNode(node.left, pathSums);
+        int rightMaxSum = maxPathSumFromNode(node.right, pathSums);
+        leftMaxSum = leftMaxSum > 0 ? leftMaxSum : 0;
+        rightMaxSum = rightMaxSum > 0 ? rightMaxSum : 0;
+        pathSums.add(leftMaxSum + node.val + rightMaxSum);
+        return leftMaxSum > rightMaxSum ? node.val + leftMaxSum : node.val + rightMaxSum;
     }
 }
