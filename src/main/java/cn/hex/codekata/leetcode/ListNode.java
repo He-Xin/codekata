1d3e1adb228a8a81b1bf7d34e67a71ff7317fe08
He-Xin <hexinmthuy@gmail.com>
2014-06-04 22:14:16 +0800

    leetcode, Swap Nodes in Pairs

--- src/main/java/cn/hex/codekata/leetcode/ListNode.java
+++ src/main/java/cn/hex/codekata/leetcode/ListNode.java
@@ -94,4 +94,37 @@ public class ListNode {
 
         return head;
     }
+
+    /**
+     * Given a linked list, swap every two adjacent nodes and return its head.
+     *
+     * For example,
+     * Given 1->2->3->4, you should return the list as 2->1->4->3.
+     *
+     * Your algorithm should use only constant space. You may not modify the values in the list, only nodes itself can be changed.
+     * @param head of linked list
+     * @return a new linked list
+     */
+    public ListNode swapPairs(ListNode head) {
+        if (head == null) return null;
+        if (head.next == null) return head;
+
+        ListNode current = head , previous = null;
+        ListNode newHead = head.next;
+
+        while (current != null) {
+            ListNode next = current.next;
+            if (next == null)
+                break;
+            current.next = next.next;
+            next.next = current;
+            if (previous != null) {
+                previous.next = next;
+            }
+            previous = current;
+            current = current.next;
+        }
+
+        return newHead;
+    }
 }
