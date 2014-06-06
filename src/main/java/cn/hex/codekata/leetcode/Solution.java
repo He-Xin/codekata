05935ea156772d78d09a60404bbdc32d2b5623fc
He-Xin <hexinmthuy@gmail.com>
2014-06-06 20:52:02 +0800

    leetcode, zig zad conversion

--- src/main/java/cn/hex/codekata/leetcode/Solution.java
+++ src/main/java/cn/hex/codekata/leetcode/Solution.java
@@ -479,4 +479,39 @@ public class Solution {
 
         return result.reverse().toString();
     }
+
+    /**
+     * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+     * <p>
+     * P   A   H   N
+     * A P L S I I G
+     * Y   I   R
+     * And then read line by line: "PAHNAPLSIIGYIR"
+     * Write the code that will take a string and make this conversion given a number of rows:
+     * <p>
+     * string convert(string text, int nRows);
+     * convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
+     */
+    public String convert(String s, int nRows) {
+        if (nRows == 1) return s;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < nRows; i++) {
+            int delta = (nRows - 1 - i) * 2;
+            if (delta == 0) {
+                delta = (nRows - 1) * 2;
+            }
+            int index = i;
+            while (index < s.length()) {
+                sb.append(s.charAt(index));
+                index += delta;
+                if (i != 0 && i != nRows - 1) {
+                    if (index < s.length())
+                        sb.append(s.charAt(index));
+
+                    index += i * 2;
+                }
+            }
+        }
+        return sb.toString();
+    }
 }
