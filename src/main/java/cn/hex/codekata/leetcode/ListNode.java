610295282c5462e0785e61720dfa382e72e9363b
He-Xin <hexinmthuy@gmail.com>
2014-06-09 22:41:20 +0800

    leetcode, merge k sorted lists

--- src/main/java/cn/hex/codekata/leetcode/ListNode.java
+++ src/main/java/cn/hex/codekata/leetcode/ListNode.java
@@ -1,5 +1,8 @@
 package cn.hex.codekata.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by hex.
  */
@@ -97,11 +100,12 @@ public class ListNode {
 
     /**
      * Given a linked list, swap every two adjacent nodes and return its head.
-     *
+     * <p>
      * For example,
      * Given 1->2->3->4, you should return the list as 2->1->4->3.
-     *
+     * <p>
      * Your algorithm should use only constant space. You may not modify the values in the list, only nodes itself can be changed.
+     *
      * @param head of linked list
      * @return a new linked list
      */
@@ -109,7 +113,7 @@ public class ListNode {
         if (head == null) return null;
         if (head.next == null) return head;
 
-        ListNode current = head , previous = null;
+        ListNode current = head, previous = null;
         ListNode newHead = head.next;
 
         while (current != null) {
@@ -126,5 +130,52 @@ public class ListNode {
         }
 
         return newHead;
+    }
+
+    //Merge k sorted lists
+    public static ListNode mergeKLists(List<ListNode> lists) {
+        if (lists.size() == 0) return null;
+        if (lists.size() == 1) return lists.get(0);
+
+        while (lists.size() > 1) {
+            List<ListNode> temp = new ArrayList<>();
+            int i = 0;
+            while (i < lists.size()) {
+                ListNode merged = lists.get(i);
+                i++;
+                if (i < lists.size()) {
+                    merged = mergeTwoLists(merged, lists.get(i));
+                    i++;
+                }
+                temp.add(merged);
+            }
+            lists = temp;
+        }
+        return lists.get(0);
+    }
+
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode current = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                current.next = l1;
+                current = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                current = l2;
+                l2 = l2.next;
+            }
+        }
+
+        if (l1 == null) {
+            current.next = l2;
+        } else {
+            //l2 is null
+            current.next = l1;
+        }
+
+        return head.next;
     }
 }
