d23f2b9ec5373bcf5b5b9c3d2616b0ef30164cc9
He-Xin <hexinmthuy@gmail.com>
2014-06-10 20:56:25 +0800

    leetcode, remove Nth node From End

--- src/main/java/cn/hex/codekata/leetcode/ListNode.java
+++ src/main/java/cn/hex/codekata/leetcode/ListNode.java
@@ -255,4 +255,21 @@ public class ListNode {
         }
         return i;
     }
+
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode node1 = head, node2 = head;
+        for (int i = 0; i< n;i++) {
+            node1 = node1.next;
+        }
+
+        if (node1 == null) return head.next;
+
+        while (node1.next != null) {
+            node1 = node1.next;
+            node2 = node2.next;
+        }
+
+        node2.next = node2.next.next;
+        return head;
+    }
 }
