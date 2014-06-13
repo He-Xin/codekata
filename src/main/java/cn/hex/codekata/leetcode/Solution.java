f813470f4a4e009db4753e2c51f6cfeae72eaac8
He-Xin <hexinmthuy@gmail.com>
2014-06-13 21:49:24 +0800

    leetcode, Longest Common Prefix

--- src/main/java/cn/hex/codekata/leetcode/Solution.java
+++ src/main/java/cn/hex/codekata/leetcode/Solution.java
@@ -578,4 +578,22 @@ public class Solution {
 
         return stack.empty();
     }
+
+    //Longest Common Prefix
+    //Write a function to find the longest common prefix string amongst an array of strings.
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+        if (strs.length == 1) return strs[0];
+        StringBuilder sb= new StringBuilder();
+        for (int i=0; i<strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+            for (int j=1; j < strs.length; j++) {
+                if (strs[j].length() <= i || strs[j].charAt(i) != c) {
+                    return sb.toString();
+                }
+            }
+            sb.append(c);
+        }
+        return sb.toString();
+    }
 }
