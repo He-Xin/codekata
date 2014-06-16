f6ede4a9e896152b888bd6364532bbe06132d23b
He-Xin <hexinmthuy@gmail.com>
2014-06-16 21:21:17 +0800

    leetcode, climb Stairs

--- src/main/java/cn/hex/codekata/leetcode/Solution.java
+++ src/main/java/cn/hex/codekata/leetcode/Solution.java
@@ -626,4 +626,19 @@ public class Solution {
 
         return -1;
     }
+
+    //You are climbing a stair case. It takes n steps to reach to the top.
+    //Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+    //fibonacci sequence
+    public static int climbStairs(int n) {
+        int n1 = 1, n2 = 2, count = 0;
+        if (n==1) return 1;
+        if (n==2) return 2;
+        for (int i=3; i<=n; i++) {
+            count = n1 + n2;
+            n1 = n2;
+            n2 = count;
+        }
+        return count;
+    }
 }
