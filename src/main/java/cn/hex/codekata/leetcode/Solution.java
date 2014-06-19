a860065f9a9dd0d809480c1d77122243b0c6d98d
He-Xin <hexinmthuy@gmail.com>
2014-06-19 20:29:37 +0800

    leetcode, Minimum Path Sum

--- src/main/java/cn/hex/codekata/leetcode/Solution.java
+++ src/main/java/cn/hex/codekata/leetcode/Solution.java
@@ -675,4 +675,34 @@ public class Solution {
         list.addAll(results);
         return list;
     }
+
+    // Minimum Path Sum
+    // Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+    // Note: You can only move either down or right at any point in time.
+    public static int minPathSum(int[][] grid) {
+        int m = grid.length;
+
+        if (m == 0) return 0;
+
+        int n = grid[0].length;
+
+        if (n == 0) return 0;
+
+        for (int i = 1; i < n; i++) {
+            grid[0][i] = grid[0][i] + grid[0][i - 1];
+        }
+
+        for (int i = 1; i < m; i++) {
+            grid[i][0] = grid[i][0] + grid[i - 1][0];
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                int sum = grid[i - 1][j] < grid[i][j - 1] ? grid[i - 1][j] : grid[i][j - 1];
+                grid[i][j] = grid[i][j] + sum;
+            }
+        }
+
+        return grid[m - 1][n - 1];
+    }
 }
