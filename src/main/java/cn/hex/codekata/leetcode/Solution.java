78a36eeca9529ec22d6b0b8925b6c49e35aa497b
He-Xin <hexinmthuy@gmail.com>
2014-07-22 22:05:56 +0800

    leetcode, Set Matrix Zeroes

--- src/main/java/cn/hex/codekata/leetcode/Solution.java
+++ src/main/java/cn/hex/codekata/leetcode/Solution.java
@@ -836,4 +836,51 @@ public class Solution {
         }
         return sum;
     }
+
+    //Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in place.
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        if (m==0) return;
+        int n = matrix[0].length;
+        if (n==0) return;
+
+
+        boolean row0 = false;
+        boolean col0 = false;
+
+        for (int[] row : matrix) {
+            if (row[0] == 0) {
+                col0 = true;
+            }
+        }
+
+        for (int i=0;i<n;i++) {
+            if (matrix[0][i] == 0) {
+                row0 = true;
+            }
+        }
+
+        for (int i=1; i<m; i++) {
+            for (int j=1;j<n;j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+
+
+        for (int i=1; i<m;i++) {
+            for (int j=1; j<n;j++ ) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            }
+        }
+
+        if (col0)
+            for (int i=0;i<m;i++) matrix[i][0] = 0;
+        if (row0)
+            for (int i=0;i<n;i++) matrix[0][i] = 0;
+
+    }
 }
