0fcc67eb041f5464971897be110c480c336d9d2d
He-Xin <hexinmthuy@gmail.com>
2014-08-12 22:35:10 +0800

    leetcode, Letter Combinations of a Phone Number

--- src/main/java/cn/hex/codekata/leetcode/Solution.java
+++ src/main/java/cn/hex/codekata/leetcode/Solution.java
@@ -1083,4 +1083,41 @@ public class Solution {
 
         return neg ? 1/result : result;
     }
+
+    //Letter Combinations of a Phone Number
+    public static List<String> letterCombinations(String digits) {
+        Map<Character, List<String>> map = new HashMap<>();
+        map.put('2', Arrays.asList("a", "b", "c"));
+        map.put('3', Arrays.asList("d", "e", "f"));
+        map.put('4', Arrays.asList("g", "h", "i"));
+        map.put('5', Arrays.asList("j", "k", "l"));
+        map.put('6', Arrays.asList("m", "n", "o"));
+        map.put('7', Arrays.asList("p", "q", "r", "s"));
+        map.put('8', Arrays.asList("t", "u", "v"));
+        map.put('9', Arrays.asList("w", "x", "y", "z"));
+
+        List<String> results = new ArrayList<>();
+
+        if (digits.length()==0) {
+            results.add("");
+        }
+
+        for (int i=0; i<digits.length(); i++) {
+            char digit = digits.charAt(i);
+            List<String> chars = map.get(digit);
+            if (results.size() == 0) {
+                results = chars;
+                continue;
+            }
+            List<String> temp = new ArrayList<>();
+            for (String r : results) {
+                for (String c: chars) {
+                    temp.add(r+c);
+                }
+            }
+            results = temp;
+        }
+
+        return results;
+    }
 }
